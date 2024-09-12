5a1fc8a49b68622b9fbca721868ac41abc72b749
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-09-12 01:00:13 -0400

    Create guestLoginForm.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

---
+++ widgets/views/guestLoginForm.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $model \humhub\modules\user\models\forms\Login */
+?>
+
+<div class="panel panel-default">
+    <div class="panel-body">
+        <?php $form = ActiveForm::begin([
+            'id' => 'guest-login-form',
+            'action' => ['/user/auth/login'],  // Point to HumHub's core AuthController
+            'method' => 'post',
+        ]); ?>
+
+        <?= $form->field($model, 'username')->textInput(['placeholder' => Yii::t('UserModule.auth', 'username or email')]); ?>
+
+        <?= $form->field($model, 'password')->passwordInput(['placeholder' => Yii::t('UserModule.auth', 'password')]); ?>
+
+        <?= $form->field($model, 'rememberMe')->checkbox(); ?>
+
+        <?= Html::submitButton(Yii::t('UserModule.auth', 'Sign in'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']); ?>
+
+        <?php ActiveForm::end(); ?>
+    </div>
+</div>
